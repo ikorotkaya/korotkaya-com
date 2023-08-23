8de20f0897db54b653bc21fdc6e9f1d4d3a8ce75
ikorotkaya <irinkakorotkaya@gmail.com>
2023-08-23 11:45:52 +0200

    Change css files to scss, create page container and navbar with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__container">
+        <div className="app__navbar">
+          <div className="navbar__logo">Irina Korotkaya</div>
+          <div className="navbar__links">
+            <a className='navbar__link' href="#about">about</a>
+            <a className='navbar__link' href="#projects">projects</a>
+            <a className='navbar__link' href="#contact">contact</a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
