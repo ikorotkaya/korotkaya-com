a6216e4e95ad688298bf1d9653c3f77c82bd9aa2
ikorotkaya <irinkakorotkaya@gmail.com>
2023-08-23 23:02:11 +0200

    Add about me section with style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import avatar from "./images/Irina_pic.png";
 
 export default function App() {
   return (
@@ -8,12 +9,43 @@ export default function App() {
         <div className="app__navbar">
           <div className="navbar__logo">Irina Korotkaya</div>
           <div className="navbar__links">
-            <a className='navbar__link' href="#about">about</a>
-            <a className='navbar__link' href="#projects">projects</a>
-            <a className='navbar__link' href="#contact">contact</a>
+            <a className="navbar__link" href="#about">
+              about
+            </a>
+            <a className="navbar__link" href="#projects">
+              projects
+            </a>
+            <a className="navbar__link" href="#contact">
+              contact
+            </a>
+          </div>
+        </div>
+        <div className="app__topics">
+          <div className="app__topic">
+            <div className="topic__about-me" id="about">
+              <img
+                className="about-me__avatar"
+                src={String(avatar)}
+                alt="Irina
+                Korotkaya"
+              />
+              <div className="about-me__description">
+                <div className="description__headline">
+                  Hey, I'm{" "}
+                  <b>
+                    <i>Irina</i>
+                  </b>
+                  !
+                </div>
+                <div className="description__text">
+                  I'm a software developer with a background in architecture and
+                  design.
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
