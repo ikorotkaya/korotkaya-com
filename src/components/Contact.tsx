a5a8c51bf4b8f3cf143d09cedbacc0d68082184c
ikorotkaya <irinkakorotkaya@gmail.com>
2023-08-24 23:06:11 +0200

    Change font family, change black color of text, add contact section headline

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,8 +2,8 @@ export function Contact() {
   return (
     <div className="app__topic">
       <div className="topic__contact" id="contact">
+        <div className="contact__headline topic-headline">Contact</div>
       </div>
     </div>
-
-  )
+  );
 }
