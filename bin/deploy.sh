bbc53f77d92ac19f1d0870b0c4583642d447b2f5
ikorotkaya <irinkakorotkaya@gmail.com>
2023-08-26 20:14:20 +0200

    Copy the contents of a new build folder to the root of the production branch

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,6 +8,13 @@ git checkout production
 
 git checkout main -- build/*
 
+# Deletes all files except build folder
+shopt -s extglob
+rm -rf !build
+shopt -u extglob
+
+# Copies files from the build folder to the root
+cp -r build/* .
 rm -rf build
 
 git add .
