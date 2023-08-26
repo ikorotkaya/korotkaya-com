ce1914b9c8f6e7eba20fc9914d0ee1795657579e
ikorotkaya <irinkakorotkaya@gmail.com>
2023-08-26 16:14:33 +0200

    Add project pics border, add email to contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,6 +3,8 @@ export function Contact() {
     <div className="app__topic">
       <div className="topic__contact" id="contact">
         <div className="contact__headline topic-headline">Contact</div>
+        <a href = "mailto: irina@korotkaya.com">Say hello! 👋</a>
+
       </div>
     </div>
   );
