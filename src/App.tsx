52964251f2e165ae377f0fca2bfab19454d23ec1
ikorotkaya <irinkakorotkaya@gmail.com>
2023-08-27 20:07:02 +0200

    Import react

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.scss";
 import { Navbar } from "./components/Navbar";
 import { About } from "./components/About";
--- src/components/DesktopNavbarLinks.tsx
+++ src/components/DesktopNavbarLinks.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 const sections = [
   { id: "about", text: "About" },
   { id: "skills", text: "Skills" },
