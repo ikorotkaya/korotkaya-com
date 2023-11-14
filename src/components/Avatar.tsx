26709b4294bd5d468d908738e526cac68145e16f
ikorotkaya <irinkakorotkaya@gmail.com>
2023-11-14 14:11:48 +0100

    Update avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-import avatar from "../images/Irina_pic.png";
-
 export function Avatar() {
   return (
     <img
       className="about-me__avatar"
-      src={String(avatar)}
+      src="../images/Irina_image.png"
       alt="Irina
   Korotkaya"
     />
