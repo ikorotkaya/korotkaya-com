c6a50c141ab80cddc55e9325cce5f3a2c6bbd308
ikorotkaya <irinkakorotkaya@gmail.com>
2024-03-18 20:44:54 +0100

    update description

--- src/components/Subheadline.tsx
+++ src/components/Subheadline.tsx
@@ -4,7 +4,7 @@ export function Subheadline() {
   return (
     <div className="description__text">
       I'm a Full Stack Engineer with a background in Design and Architecture,
-      based in Berlin. Currently looking for a full-time developer role! 🎯
+      based in Berlin. Currently working as a Software Engineer at <a href="https://www.buena.com" className='website_link'>Buena</a>.
     </div>
   );
 }
