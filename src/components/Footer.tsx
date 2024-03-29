437cb76305ec76f51b9c801ad5e242d4079bd05c
ikorotkaya <irinkakorotkaya@gmail.com>
2024-03-29 15:43:06 +0100

    Update footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,7 @@ export function Footer() {
   return (
     <div className="app__footer">
       <div className="footer__text">
-        Made with{" "}
-        <img
-          draggable="false"
-          className="footer__emoji"
-          alt="❤️"
-          src="https://twemoji.maxcdn.com/v/13.0.2/svg/2764.svg"
-        />{" "}
-        by <b>Irina Korotkaya</b> in 2023
+        Made with ♥️ by <b>Irina Korotkaya</b> in 2023
       </div>
     </div>
   );
