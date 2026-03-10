c1823e1587b7a2c365ecd99665c0ca1ce09d40bb
Irina Korotkaya <irina@buena.com>
2026-03-10 21:30:33 +0100

    Update website link

--- src/components/Subheadline.tsx
+++ src/components/Subheadline.tsx
@@ -5,7 +5,7 @@ export function Subheadline() {
     <div className="description__text">
       I'm a Software Engineer with a background in Design and Architecture,
       based in Berlin. Currently working as a Software Engineer at{" "}
-      <a href="https://www.liqid.com" className="website_link">
+      <a href="https://www.liqid.de" className="website_link">
         LIQID
       </a>
       .
